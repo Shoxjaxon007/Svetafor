69ecf4201b3261cad6f6524b59cf13dba3d6b6f6
Shoxjaxon007 <shakarovshoxjaxon666@gmail.com>
2023-08-17 17:18:59 +0500

    fix

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = prompt("Svetaforni Rangini Kiriting")
+
+if(a === "qizil"){
+    console.log("Iltimos Qizil Chiroqda To'xtab Turing")
+}
+else if(a === "yashil"){
+    console.log("Xarakatlanishingiz mumkin")
+}
+else if(a === "sariq"){
+    console.log("Iltimos Sariq Chiroqda Tayorlaning")
+}
